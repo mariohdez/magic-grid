152e14bb9b48ed158be80e05d03d849f708c5584
Mario Hernandez <mhmario963@gmail.com>
2020-07-04 16:58:43 -0700

    draw first squre.

--- magicgrid.js
+++ magicgrid.js
@@ -1,43 +1,51 @@
 'use strict';
 
+const nintyPercent = 0.9;
 const canvasId = 'magic-grid';
 const headerId = 'welcome-header';
-const contextType = '2d;'
+const contextType = '2d';
 const canvasNotSupportMessage = 'Sorry! You are using a browser that does not support canvas.';
+const contextUndefined = 'Sorry, something went wrong. Can\' draw grid.';
 
-const getCanvasWidth = (windowWidth) => {
-    const nintyPercent = 0.9;
-    return windowWidth * nintyPercent;
-};
+const drawGrid = (canvas, context) => {
+    if (!context) {
+        alert(contextUndefined);
+        return;
+    }
 
-const getCanvasHeight = (windowHeight, welcomeHeaderHeight) => {
-    const nintyPercent = 0.9;
-    return (windowHeight - welcomeHeaderHeight) * nintyPercent;
-};
+    let red = 255;
+    let white = 0;
+    let blue = 0;
+    const squareSideLength = 50;
 
-window.onresize = (event) => {
-    // Resize the canvas width and height.
-    const welcomeHeader = document.getElementById(headerId);
-    const magicGridCanvas = document.getElementById(canvasId);
+    const blueRgb = {
+        red: 0,
+        white: 0,
+        blue: 255,
+    };
 
-    magicGridCanvas.width = getCanvasWidth(window.innerWidth);
-    magicGridCanvas.height = getCanvasHeight(window.innerHeight, welcomeHeader.clientHeight);
+    const redRgb = {
+        red: 255,
+        white: 0,
+        blue: 0,
+    };
 
-    console.log(`canvas width: ${magicGridCanvas.width}, and height: ${magicGridCanvas.height}.`);
+    context.fillStyle = `rgb(${redRgb.red}, ${redRgb.white}, ${redRgb.blue})`;
+    context.fillRect(0, 0, squareSideLength, squareSideLength);
 };
 
 window.onload = () => {
     const welcomeHeader = document.getElementById(headerId);
     const magicGridCanvas = document.getElementById(canvasId);
+    const sideLength = 600;
 
     if (!magicGridCanvas.getContext) {
         alert(canvasNotSupportMessage);
     }
 
-    magicGridCanvas.width = getCanvasWidth(window.innerWidth);
-    magicGridCanvas.height = getCanvasHeight(window.innerHeight, welcomeHeader.clientHeight);
-
-    console.log(`canvas width: ${magicGridCanvas.width}, and height: ${magicGridCanvas.height}.`);
+    magicGridCanvas.width = sideLength;
+    magicGridCanvas.height = sideLength;
 
     let context = magicGridCanvas.getContext(contextType);
+    drawGrid(magicGridCanvas, context);
 };
