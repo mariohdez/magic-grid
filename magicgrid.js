d5e8b61443334e6c9e68f16436d4af433f3fa20b
Mario Hernandez <mhmario963@gmail.com>
2020-07-04 17:15:31 -0700

    added grid.

--- magicgrid.js
+++ magicgrid.js
@@ -1,11 +1,23 @@
 'use strict';
 
-const nintyPercent = 0.9;
+const sideLength = 600;
+const squareSideLength = 50;
 const canvasId = 'magic-grid';
 const headerId = 'welcome-header';
 const contextType = '2d';
 const canvasNotSupportMessage = 'Sorry! You are using a browser that does not support canvas.';
 const contextUndefined = 'Sorry, something went wrong. Can\' draw grid.';
+
+const getRandomColor = () => {
+    const max = 255;
+    const min = 0;
+    return {
+        red: Math.floor(Math.random() * (max - min + 1) + min),
+        white: Math.floor(Math.random() * (max - min + 1) + min),
+        blue: Math.floor(Math.random() * (max - min + 1) + min),
+    }
+}
+
 
 const drawGrid = (canvas, context) => {
     if (!context) {
@@ -13,31 +25,19 @@ const drawGrid = (canvas, context) => {
         return;
     }
 
-    let red = 255;
-    let white = 0;
-    let blue = 0;
-    const squareSideLength = 50;
+    const iterationLength = sideLength / squareSideLength;
 
-    const blueRgb = {
-        red: 0,
-        white: 0,
-        blue: 255,
-    };
-
-    const redRgb = {
-        red: 255,
-        white: 0,
-        blue: 0,
-    };
-
-    context.fillStyle = `rgb(${redRgb.red}, ${redRgb.white}, ${redRgb.blue})`;
-    context.fillRect(0, 0, squareSideLength, squareSideLength);
+    for (let j = 0; j < iterationLength; ++j) {
+        for (let i = 0; i < iterationLength; ++i) {
+            let color = getRandomColor();
+            context.fillStyle = `rgb(${color.red}, ${color.white}, ${color.blue})`;
+            context.fillRect(i * squareSideLength, j*squareSideLength, squareSideLength, squareSideLength);
+        }
+    }
 };
 
 window.onload = () => {
-    const welcomeHeader = document.getElementById(headerId);
     const magicGridCanvas = document.getElementById(canvasId);
-    const sideLength = 600;
 
     if (!magicGridCanvas.getContext) {
         alert(canvasNotSupportMessage);
